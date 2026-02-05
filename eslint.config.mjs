f33062edbca3eb5bb867786c773e20ee146ceb55
cay0716 <cay07162769@gmail.com>
2026-02-05 10:58:25 +0900

    🔧 conf: add prettier and eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,12 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "prefer-const": "warn", // 변수가 재할당 되지 않는다면 let 대신 const 사용하도록 경고
+      "no-redeclare": "warn", // 변수 중복 선언시 경고
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
