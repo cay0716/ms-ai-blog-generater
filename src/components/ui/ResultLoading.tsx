b42c7f694ee0cfd4d46d42fc619369d4043f7949
cay0716 <cay07162769@gmail.com>
2026-02-11 09:03:21 +0900

    💄style: change location

--- src/components/ui/ResultLoading.tsx
+++ src/components/ui/ResultLoading.tsx
@@ -1,6 +1,6 @@
-export default function Loading() {
+export default function ResultLoading() {
   return(
-    <div className="flex justify-left gap-2 py-8">
+    <div className="flex justify-center gap-2 py-8">
       <span className="loading-dot" />
       <span className="loading-dot delay-100" />
       <span className="loading-dot delay-200" />
